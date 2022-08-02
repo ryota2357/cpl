19f4293984c08df9d2bc6097516476275c9d19d8
2357 <61523777+ryota2357@users.noreply.github.com>
2022-08-02 13:00:44 +0900

    add: prime_sieve

---
+++ lib/prime_sieve/index.cpp
@@ -0,0 +1,62 @@
+#include "../template/index.cpp"
+
+// [START]
+struct PrimeSieve
+{
+    const uint __size;
+    vector<int> sieve, primes;
+    PrimeSieve(const int n = 1) : __size(max(1, n)), sieve(max(2, n + 1)) {
+        sieve[0] = sieve[1] = -1;
+        primes.emplace_back(2);
+        for (int i = 2; i <= n; i += 2) sieve[i] = 2;
+        for (ll i = 3; i <= n; i += 2) {
+            if (sieve[i]) continue;
+            primes.emplace_back(i);
+            sieve[i] = i;
+            for (ll j = i * i; j <= n; j += i)
+                if (!sieve[j]) sieve[j] = i;
+        }
+    }
+    inline bool isPrime(const int& x) const {
+        assert((uint)x <= __size);
+        return sieve[x] == x;
+    }
+    vector<int> factorize(int x) const {
+        assert((uint)x <= __size);
+        vector<int> ret;
+        if (x < 2) return ret;
+        while (x != 1) {
+            ret.emplace_back(sieve[x]);
+            x /= sieve[x];
+        }
+        return ret;
+    }
+    int divisorCount(int x) const {
+        assert((uint)x <= __size);
+        if (x == 0) return 0;
+        int cnt = 1, ret = 1;
+        while (x != 1) {
+            const int b = sieve[x];
+            x /= sieve[x];
+            const int p = sieve[x];
+            if (b == p) cnt++;
+            else ret *= (cnt + 1), cnt = 1;
+        }
+        ret *= cnt;
+        return ret;
+    }
+    vector<int> divisors(const int x) const {
+        assert((uint)x <= __size);
+        if (x == 0) return {};
+        auto fac = factorize(x);
+        vector<int> ret = {1};
+        for (const int& f : fac) {
+            int s = ret.size();
+            for (int i = 0; i < s; i++) ret.emplace_back(ret[i] * f);
+        }
+        sort(ret.begin(), ret.end());
+        ret.erase(unique(ret.begin(), ret.end()), ret.end());
+        return ret;
+    }
+};
+// [END]
---
+++ lib/prime_sieve/test.cpp
@@ -0,0 +1,61 @@
+#include <vector>
+#include "index.cpp"
+#include "../_test_util/util.cpp"
+
+int main() {
+    test::Start("prime_sieve");
+    const int n = 1000000;
+    { /* isPram */
+        PrimeSieve seive(n);
+        const auto sqrtJudge = [&](int x){
+            if(x == 2) return true;
+            if (x < 2 || x % 2 == 0) return false;
+            for(ll i = 3; i*i <= x; i += 2) {
+                if(x%i) continue;
+                return false;
+            }
+            return true;
+        };
+        rep(i, 0, n) eq2(seive.isPrime(i), sqrtJudge(i), i);
+    }
+    { /* factorize */
+        PrimeSieve seive(n);
+        const auto sqrtJudge = [&](int x) {
+            vector<int> ret;
+            if(x < 2) return ret;
+            while(x % 2 == 0) {
+                ret.emplace_back(2);
+                x /= 2;
+            }
+            for(ll i = 3; i * i <= n; i += 2) {
+                while(x % i == 0) {
+                    ret.emplace_back(i);
+                    x /= i;
+                }
+            }
+            if(x != 1) ret.emplace_back(x);
+            return ret;
+        };
+        rep(i, 0, n) eq2(seive.factorize(i), sqrtJudge(i), i);
+    }
+    { /* divisors */
+        PrimeSieve seive(n);
+        const auto sqrtJudge = [&](int x) {
+            vector<int> ret;
+            if(x == 0) return ret;
+            for(ll i = 1; i * i <= x; i++) {
+                if(x % i) continue;
+                ret.push_back(i);
+                if (x / i != i) ret.push_back(x / i);
+            }
+            Sort(ret);
+            return ret;
+        };
+        rep(i, 0, n) eq2(seive.divisors(i), sqrtJudge(i), i);
+    }
+    { /* divisorCount */
+        PrimeSieve seive(n);
+        rep(i, 0, n) eq2(seive.divisorCount(i), seive.divisors(i).size(), i);
+    }
+    test::Finish();
+}

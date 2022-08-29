c47f82f854cd302eed51cdda1cbdc93936452116
2357 <61523777+ryota2357@users.noreply.github.com>
2022-08-29 20:54:22 +0900

    move

--- lib/sparse_table/test.cpp
+++ lib/sparse_table/test.cpp
@@ -41,3 +41,24 @@ int main() {
     }
     test::Finish();
 }
+
+/* https://atcoder.jp/contests/abc263/tasks/abc263_d
+int main() {
+    ll n, l, r; IN(n, l, r);
+    vector<ll> a(n); IN(a);
+    vector<ll> b(n+1); rep(i, 0, n) b[i+1] = b[i] + a[i];
+    vector<ll> rr(n+1); rr[0] = r * n;
+    rep(i, 0, n) {
+        rr[i+1] = rr[i] - r + a[i];
+    }
+    SparseTable<ll, min> table(rr);
+    ll ans = rr[n];
+    rep(i, 0, n+1) {
+        ll left = l * i;
+        ll right = table.get(i, n+1) - (b[i] - b[0]);
+        chmin(ans, left + right);
+    }
+    OUT(ans);
+    return 0;
+}
+*/

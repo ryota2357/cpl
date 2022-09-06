9985b6518bcf640244f84ff51f79ddc01f5d71f0
2357 <ryoooo2357g@gmail.com>
2022-09-06 11:07:03 +0900

    add: topological_sort

---
+++ lib/topological_sort/index.cpp
@@ -0,0 +1,31 @@
+#include "../template/index.cpp"
+
+#ifndef LIB_TOPOLOGICAL_SORT
+#define LIB_TOPOLOGICAL_SORT
+
+// [START]
+vector<int> topologicalSort(const vector<vector<int>>& graph) {
+    vector<int> ret;
+    const int size = graph.size();
+    vector<int> indegree(size);
+    for (const vector<int>& v : graph) {
+        for (const int& to : v) indegree[to]++;
+    }
+    queue<int> que;
+    for (int i = 0; i < size; i++) {
+        if (indegree[i] == 0) que.emplace(i);
+    }
+    while (que.size()) {
+        const int v = que.front();
+        que.pop();
+        ret.emplace_back(v);
+        for (const int& to : graph[v]) {
+            indegree[to]--;
+            if (indegree[to] == 0) que.emplace(to);
+        }
+    }
+    return ret;
+}
+// [END]
+
+#endif

829ec81e815cc3a6000639306151507bd05e722d
GitHub <noreply@github.com>
2023-04-18 00:27:21 +0000

    Update deno dependencies

--- denops/@ddu-kinds/cpl.ts
+++ denops/@ddu-kinds/cpl.ts
@@ -3,8 +3,8 @@ import {
   Actions,
   BaseKind,
   DduItem,
-} from "https://deno.land/x/ddu_vim@v2.7.0/types.ts";
-import { Denops, fn } from "https://deno.land/x/ddu_vim@v2.7.0/deps.ts";
+} from "https://deno.land/x/ddu_vim@v2.8.3/types.ts";
+import { Denops, fn } from "https://deno.land/x/ddu_vim@v2.8.3/deps.ts";
 import { ActionData } from "../@ddu-sources/cpl.ts";
 
 type Params = Record<never, never>;

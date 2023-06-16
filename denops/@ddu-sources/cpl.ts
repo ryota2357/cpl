8a3fc398d9b3003b43c1c8c22f1abda65a9bf43d
GitHub <noreply@github.com>
2023-06-16 00:33:36 +0000

    Update deno dependencies

--- denops/@ddu-sources/cpl.ts
+++ denops/@ddu-sources/cpl.ts
@@ -1,6 +1,6 @@
-import { BaseSource, Item } from "https://deno.land/x/ddu_vim@v3.0.2/types.ts";
-import { Denops } from "https://deno.land/x/ddu_vim@v3.0.2/deps.ts";
-import { join, resolve } from "https://deno.land/std@0.191.0/path/mod.ts";
+import { BaseSource, Item } from "https://deno.land/x/ddu_vim@v3.1.0/types.ts";
+import { Denops } from "https://deno.land/x/ddu_vim@v3.1.0/deps.ts";
+import { join, resolve } from "https://deno.land/std@0.192.0/path/mod.ts";
 
 type Params = Record<never, never>;
 

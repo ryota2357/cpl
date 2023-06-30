efbb21c77f45dff4351e249ac4a554d3d72971a9
2357 <61523777+ryota2357@users.noreply.github.com>
2023-06-30 15:06:44 +0900

    remove old test files

--- lib/_test_runner/runner.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd `dirname $0`
-
-for target in $(find ../*/local.test.hpp)
-do
-    clang++ -std=c++17 -stdlib=libc++ -Wall -O2 $target
-    ./a.out
-done
--- lib/_test_util/index.cpp
+++
@@ -1,9 +0,0 @@
-#include "../_test_util/util.cpp"
-
-int main() {
-    test::Start("");
-    {
-        test::Require("");
-    }
-    test::Finish();
-}
